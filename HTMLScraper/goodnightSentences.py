c765dd89f4fac7d77e0682f80de9d907a89d0b92
Ben Bowman <benbowman314@gmail.com>
2016-03-20 00:54:17 -0400

    150 Journal entries from nightlyjournals.com

---
+++ HTMLScraper/goodnightSentences.py
@@ -0,0 +1,24 @@
+import soup
+import unicodedata
+from parse import *
+
+#f = open('journals.txt', 'w')
+
+with open('nightlyurls.txt') as f:
+    for i, line in enumerate(f):
+		entry_file = open('./Journals/goodnight' + str(i), 'w')
+		current_entry = soup.URLToText(line)
+		sents = nltk.sent_tokenize(current_entry) 
+		for sentence in sents:
+				entry_file.write(unicodedata.normalize('NFKD', sentence).encode('utf8', 'ignore'))
+				entry_file.write('\n')
+		entry_file.close()
+
+'''
+journal_entry = soup.URLToText("https://www.goodnightjournal.com/2016/03/so-i-quit-my-job-today/")
+sents = nltk.sent_tokenize(journal_entry) 
+for sentence in sents:
+		f.write(unicodedata.normalize('NFKD', sentence).encode('utf8', 'ignore'))
+		f.write('\n')
+f.close()
+'''
--- HTMLScraper/soup.py
+++ HTMLScraper/soup.py
@@ -2,7 +2,11 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def URLToText(url):
-	response = urllib2.urlopen(url)
+	try:
+		request = urllib2.Request(url)
+		response = urllib2.urlopen(request)
+	except Exception:
+		return -1
 	soup = BeautifulSoup(response, 'html.parser')
 	result = soup.find('div', {'class' :'entry-content'}).text
 	return result
