6766635f5b53f439ea0e576465d5b8f1056b5f4a
Myron Leung <myronleung@gmail.com>
2016-03-23 21:11:37 -0400

    abstracted code

--- api_NLC.py
+++ api_NLC.py
@@ -8,29 +8,31 @@ natural_language_classifier = NaturalLanguageClassifierV1(
   username=cred['usr'],
   password=cred['pwd'])
 
-def createClassifier():
+def createClassifier(filename):
     print 'creating classfier...'
-    with open('Journals/Sentences/test_file_5.csv', 'rb') as training_data:
+    with open(filename, 'rb') as training_data:
         classifier = natural_language_classifier.create(
             training_data=training_data,
             name='NDL_Classfier',
             language='en'
         )
     print(json.dumps(classifier, indent=2))
-def listClassifier():
+
+def listClassifier(classifier_index):
     print 'listing classfiers...'
     classifiers = natural_language_classifier.list()
     output = json.dumps(classifiers, indent=2)
     print json.dumps(classifiers, indent=2)
     output = json.loads(output)
-    print output['classifiers'][0]['classifier_id']
-    return output['classifiers'][0]['classifier_id']
+    print output['classifiers'][classifier_index]['classifier_id']
+    return output['classifiers'][classifier_index]['classifier_id']
+
 def checkClassifierStatus(classifier_id):
     print 'check classifier status...'
     status = natural_language_classifier.status(classifier_id)
     print (json.dumps(status, indent=2))
 
 
-# createClassifier()
-classifier_id = listClassifier()
+# createClassifier('Journals/Sentences/test_file_5.csv')
+classifier_id = listClassifier(0)
 checkClassifierStatus(classifier_id)
