2a14433c9b2eb65cf5d004a3e8910df64958bde4
Myron Leung <myronleung@gmail.com>
2016-03-24 14:43:35 -0400

    added function to classify whole journal

--- api_NLC.py
+++ api_NLC.py
@@ -1,4 +1,6 @@
 import json
+import pprint
+import csv
 from get_cred import get_cred
 from watson_developer_cloud import NaturalLanguageClassifierV1
 
@@ -34,8 +36,28 @@ def checkClassifierStatus(classifier_id):
 
 def classifySentence(sentence, classifier_id):
     classes = natural_language_classifier.classify(classifier_id, sentence)
-    print(json.dumps(classes, indent=2))
+    return [json.loads(json.dumps(classes, indent=2))['classes'][0]['class_name'], json.loads(json.dumps(classes, indent=2))['classes'][0]['confidence']]
+
+def classifyJournal(filename, classifier_id):
+    journal = open(filename, 'r')
+    with open(str(filename)+'_classed.csv', 'w') as csvfile:
+        fieldnames = ['Sentence', 'Class', 'Confidence']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        c = 0
+        classed = []
+        for i in journal:
+            c += 1
+            print str(i)
+            try:
+                classed = classifySentence(str(i), classifier_id)
+                writer.writerow({'Sentence': i, 'Class': classed[0], 'Confidence': classed[1]})
+            except:
+                print 'error'
+    journal.close()
 # createClassifier('Journals/Sentences/test_file_5.csv')
 classifier_id = listClassifier(0)
-checkClassifierStatus(classifier_id)
-classifySentence('I\'m really glad she\'s came out for skating and becoming part of the skating family Well', classifier_id)
+# checkClassifierStatus(classifier_id)
+# pprint.pprint(classifySentence('Kind of feel little uncomfortable here.', classifier_id))
+
+classifyJournal('Journals/CaliforniaDream/goodnight277', classifier_id)
