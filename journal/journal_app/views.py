84bee04d14f6503f47c339f1a02b130231d1c12b
Myron Leung <myronleung@gmail.com>
2016-03-25 20:01:10 -0400

    built out views for index, detail, and record_entry

--- journal/journal_app/views.py
+++ journal/journal_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
+from .forms import EntryForm
 from .models import JournalEntry
 
 # Create your views here.
@@ -15,6 +18,53 @@ def detail(request, journal_entry_id):
     entry = get_object_or_404(JournalEntry, pk=journal_entry_id)
     return render(request, 'journal_app/detail.html', {'entry': entry})
 
-def recordentry(request, journal_entry_id):
-    response = "You're looking to record entry %s"
-    return HttpResponse(response % journal_entry_id)
+def record_entry(request):
+    template = loader.get_template('journal_app/record_entry.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EntryForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            title = form.cleaned_data['title']
+            author = form.cleaned_data['author']
+            body = form.cleaned_data['body']
+            pub_date = timezone.now()
+
+            j = JournalEntry(title = title, author = author, body = body, pub_date = pub_date)
+            j.save()
+            # redirect to a new URL:
+            journal_entry_list = JournalEntry.objects.all()
+            return HttpResponseRedirect(reverse('journal_app:index', args=(journal_entry_list,)))
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EntryForm()
+
+    return render(request, 'journal_app/record_entry.html', {'form': form})
+
+def submit_entry(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EntryForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            title = form.cleaned_data['title']
+            author = form.cleaned_data['author']
+            body = form.cleaned_data['body']
+            pub_date = timezone.now()
+
+            j = JournalEntry(title = title, author = author, body = body, pub_date = pub_date)
+            j.save()
+            # redirect to a new URL:
+            journal_entry_list = JournalEntry.objects.all()
+            return HttpResponseRedirect(reverse('journal_app:index'))
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EntryForm()
+
+    return render(request, 'journal_app/record_entry.html', {'form': form})
