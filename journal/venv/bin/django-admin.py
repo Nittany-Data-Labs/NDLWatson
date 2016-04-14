477e415144224807e107b79ccf73d37aea989fcf
Myron Leung <myronleung@gmail.com>
2016-04-13 22:39:01 -0400

    commit

--- journal/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/myronleung/Documents/Development/NDL/NDLgit/NDLWatson/journal/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
